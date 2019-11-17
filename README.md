d6f81b011a1bcc5fa857c922fb6406973738f117
Carlos Precioso <kalprestito@gmail.com>
2019-11-17 03:40:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# micro-hostname
+
+Adapted the
+[`express.js` algorithm](https://github.com/expressjs/express/blob/b8e50568af9c73ef1ade434e92c60d389868361d/lib/request.js#L427)
+to a [`micro`](https://github.com/zeit/micro) middleware.
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { IncomingMessage, ServerResponse } from "http"
+
+export interface Options {
+  trustProxy: boolean | ((remoteAddress: string | undefined) => boolean)
+}
+
+const hostname = <I extends IncomingMessage>(
+  innerListener: (req: I & { hostname?: string }, res: ServerResponse) => void,
+  options?: Partial<Options>
+): ((req: I, res: ServerResponse) => void) => {
+  const _options: Options = { trustProxy: false, ...options }
+  const trustFn =
+    typeof _options.trustProxy === "function"
+      ? _options.trustProxy
+      : () => _options.trustProxy
+
+  const getHostname = (req: IncomingMessage) => {
+    let host = req.headers["x-forwarded-host"] as string | undefined
+    if (!host || !trustFn(req.connection.remoteAddress)) {
+      host = req.headers["host"]
+    } else if (host.indexOf(",") !== -1) {
+      // Note: X-Forwarded-Host is normally only ever a
+      //       single value, but this is to be safe.
+      host = host.substring(0, host.indexOf(",")).trimRight()
+    }
+    if (!host) return
+    const offset = host[0] === "[" ? host.indexOf("]") + 1 : 0
+    const index = host.indexOf(":", offset)
+    return index !== -1 ? host.substring(0, index) : host
+  }
+
+  return (req, res) => {
+    return innerListener(
+      Object.assign(req, {
+        hostname: getHostname(req)
+      }),
+      res
+    )
+  }
+}
+
+export default hostname
